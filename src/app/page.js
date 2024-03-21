8bd8b75ab67189788c032c09979a494fc6babfa3
rishabh <hespnod2348@gmai.com>
2024-03-21 20:55:01 +0530

    all changed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className=" bg-gray-950 h-[100vh] w-[100vw] flex items-center justify-center">
+      <button className=" text-slate-900 px-5 py-2 rounded-l hover:bg-white shadow-sm hover:scale-105 shadow-white bg-slate-100 font-bold">
+        <Link href='/users'>Users List</Link>
+      </button>
+    </div>
   )
 }
